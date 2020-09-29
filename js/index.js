f0d72f0eacff87b169e049132b7a9cf487c53f62
duointherain <duointherain@gmail.com>
2020-09-29 15:09:25 -0700

    menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+
+// let test = document.getElementById("nav");
+
+let navItems = document.getElementsByClassName(`nav-link`)
+
+  navItems[0].addEventListener('mouseover', function(event)  {
+    event.target.style.color = "hsl(38, 99%, 65%)"
+})
+
+navItems[0].addEventListener('mouseout', function(event)  {
+  event.target.style.color = "hsl(357, 54%, 51%)"
+})
+
+navItems[1].addEventListener('mouseover', function(event)  {
+  event.target.style.color = "hsl(38, 99%, 65%)"
+})
+
+navItems[1].addEventListener('mouseout', function(event)  {
+event.target.style.color = "hsl(357, 54%, 51%)"
+})
+navItems[2].addEventListener('mouseover', function(event)  {
+  event.target.style.color = "hsl(38, 99%, 65%)"
+})
+
+navItems[2].addEventListener('mouseout', function(event)  {
+event.target.style.color = "hsl(357, 54%, 51%)"
+})
+
+navItems[3].addEventListener('mouseover', function(event)  {
+  event.target.style.color = "hsl(38, 99%, 65%)"
+})
+
+navItems[3].addEventListener('mouseout', function(event)  {
+event.target.style.color = "hsl(357, 54%, 51%)"
+})
+
+
+
+
